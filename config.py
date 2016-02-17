a97800dd26817e12734602fde3dafb032dee47dd
RoryGee <rorygee@outlook.com>
2016-02-17 20:19:51 +0000

    Added initial configuration file

---
+++ config.py
@@ -0,0 +1,5 @@
+HOST = "irc.twitch.tv"              # Twitch IRC server address
+PORT = 6667                         # Port number, for Twitch/IRC use 6667 ~ONLY~
+NICK = "username"                   # Host Twitch username, lowercase
+PASS = "oauth:xxxxxxxxxxxxxxxxxxxx" # Host Twitch OAuth token
+CHAN = "#channel"                   # Connecting channel name
