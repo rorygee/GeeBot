24692639892ae6c43ce2931e8119623c2dc19d26
RoryGee <rorygee@outlook.com>
2016-02-18 00:14:42 +0000

    fixed formatting of chat function

--- main.py
+++ main.py
@@ -5,13 +5,6 @@ import re
 
 CHAT_MSG = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
 
-def chat(sock, msg):
-	sock.send("PRIVMSG #{} :{}".format(config.CHAN, msg))
-def ban(sock, user):
-	chat(sock, ".ban {}".format(user))
-def timeout(sock, user, secs=60):
-	chat(sock, ".timeout {}".format(user, secs))
-
 try:
 	s = socket.socket()
 	s.connect((config.HOST, config.PORT))
@@ -24,14 +17,23 @@ except Exception as e:
 	print(str(e))
 	connected = False	# Socket connection failed
 
+def chat(msg):
+	s.send(bytes('PRIVMSG %s :%s\r\n' % (config.CHAN, msg), 'UTF-8'))
+
+def ban(sock, user):
+	chat(".ban {}".format(user))
+
+def timeout(sock, user, secs=60):
+	chat(".timeout {}".format(user, secs))
+
 def active_loop():
 	while connected:
 		response = s.recv(1024).decode("utf-8")
-		if response == "PING :tmi.twitch.tv\r\n":
-			s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
-			print("Pong")
+		if response == "PING :tmi.twitch.tv\r\n":	# Detecting ping
+			s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))	# Sending pong
+			print(config.NICK + ": Pong")	# Showing pong in console
 		else:
-			username = re.search(r"\w+", response).group(0)  # Return the entire match
+			username = re.search(r"\w+", response).group(0)
 			message = CHAT_MSG.sub("", response)
 			print(username + ": " + message)
 			time.sleep(1 / config.RATE)
