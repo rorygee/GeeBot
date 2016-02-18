369bcd47ec4fe1ced9ca5acec0343b5966c75bea
RoryGee <rorygee@outlook.com>
2016-02-18 15:45:44 +0000

    Cleaning up/modularising, almost ready to start implememnting features

---
+++ conn_handle.py
@@ -0,0 +1,35 @@
+import config
+import socket
+import time
+import re
+import main
+
+CHAT_MSG = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
+
+try:
+	s = socket.socket()
+	s.connect((config.HOST, config.PORT))
+	s.send("PASS {}\r\n".format(config.PASS).encode("utf-8"))
+	s.send("NICK {}\r\n".format(config.NICK).encode("utf-8"))
+	s.send("JOIN {}\r\n".format(config.CHAN).encode("utf-8"))
+	connected = True	# Socket is connected
+
+except Exception as e:
+	print(str(e))
+	connected = False	# Socket connection failed
+
+def active_loop():
+	while connected:
+		response = s.recv(1024).decode("utf-8")
+		if response == "PING :tmi.twitch.tv\r\n":	# Detecting ping
+			s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))	# Sending pong
+			print(config.NICK + ": Pong")	# Showing pong in console
+		else:
+			username = re.search(r"\w+", response).group(0)
+			message = CHAT_MSG.sub("", response)
+			print(username + ": " + message)
+			if re.match(config.CMDP, message[0]):
+				main.check_command(username, message)
+			time.sleep(1 / config.RATE)
+
+active_loop()
--- main.py
+++ main.py
@@ -2,23 +2,10 @@ import config
 import socket
 import time
 import re
-
-CHAT_MSG = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
-
-try:
-	s = socket.socket()
-	s.connect((config.HOST, config.PORT))
-	s.send("PASS {}\r\n".format(config.PASS).encode("utf-8"))
-	s.send("NICK {}\r\n".format(config.NICK).encode("utf-8"))
-	s.send("JOIN {}\r\n".format(config.CHAN).encode("utf-8"))
-	connected = True	# Socket is connected
-
-except Exception as e:
-	print(str(e))
-	connected = False	# Socket connection failed
+import conn_handle
 
 def chat(msg):
-	s.send(bytes('PRIVMSG %s :%s\r\n' % (config.CHAN, msg), 'UTF-8'))
+	conn_handle.s.send(bytes('PRIVMSG %s :%s\r\n' % (config.CHAN, msg), 'UTF-8'))
 
 def ban(sock, user):
 	chat(".ban {}".format(user))
@@ -26,18 +13,5 @@ def ban(sock, user):
 def timeout(sock, user, secs=60):
 	chat(".timeout {}".format(user, secs))
 
-def active_loop():
-	while connected:
-		response = s.recv(1024).decode("utf-8")
-		if response == "PING :tmi.twitch.tv\r\n":	# Detecting ping
-			s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))	# Sending pong
-			print(config.NICK + ": Pong")	# Showing pong in console
-		else:
-			username = re.search(r"\w+", response).group(0)
-			message = CHAT_MSG.sub("", response)
-			print(username + ": " + message)
-			if re.match(config.CMDP, message[0]):
-				chat("Commands to be implemented in the near future")
-			time.sleep(1 / config.RATE)
-
-active_loop()
+def check_command(user, message):
+	chat("Commands to be implemented in the near future")
