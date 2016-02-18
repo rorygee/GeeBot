58d6fba20d174a78e73ffc8f5024fca236960262
RoryGee <rorygee@outlook.com>
2016-02-18 18:45:42 +0000

    minor name change

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def ban(sock, user):
 def timeout(sock, user, secs=60):
 	chat(".timeout {}".format(user, secs))
 
-def check_command(user, message):
+def valid_command(user, message):
 	command = re.search("(?<=\{0})\w+".format(config.CMDP), message)
 	if command:
 		chat("'"+command.group(0)+"' is not a valid command GeeFaceNoSpace")
