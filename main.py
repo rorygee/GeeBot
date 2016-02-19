711c6f7a50e9b7f453d40339988f3594e1cf3169
RoryGee <rorygee@outlook.com>
2016-02-19 20:26:05 +0000

    added command for returning all active moderators in chat

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import socket
 import time
 import re
 import conn_handle
+import urllib.request
+import urllib.error
+import json
 
 def chat(msg):
 	conn_handle.s.send(bytes('PRIVMSG %s :%s\r\n' % (config.CHAN, msg), 'UTF-8'))
@@ -16,6 +19,17 @@ def timeout(sock, user, secs=60):
 def valid_command(user, message):
 	command = re.search("(?<=\{0})\w+".format(config.CMDP), message)
 	if command:
-		chat("'"+command.group(0)+"' is not a valid command GeeFaceNoSpace")
+		if command.group(0) == "mods":
+			try:
+				j_obj = json.loads(urllib.request.urlopen('http://tmi.twitch.tv/group/user/rory_gee/chatters', timeout = 15).read().decode('utf-8'))
+				modsOnline = j_obj['chatters']['moderators']
+				modList = ""
+				for curMod in modsOnline:
+					modList = modList+curMod+", "
+				chat("Mods currently in chat: "+modList[0:(len(modList)-2)])
+			except urllib.error.URLError as e:
+				print(e.reason) 
+		else:
+			chat("'"+command.group(0)+"' is not a valid command GeeFaceNoSpace")
 	else:
 		chat("Put in a command dummy KappaGee")
