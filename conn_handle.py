f1445270d9830390d95b8b483a11db91ee1e4d67
RoryGee <rorygee@outlook.com>
2016-02-22 00:08:38 +0000

    fixed error in returning channel as username, implemented fetching of both

--- conn_handle.py
+++ conn_handle.py
@@ -9,8 +9,8 @@ CHAT_MSG = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
 try:
 	s = socket.socket()
 	s.connect((config.HOST, config.PORT))
-	s.send("PASS {}\r\n".format(config.PASS).encode("utf-8"))
-	s.send("NICK {}\r\n".format(config.NICK).encode("utf-8"))
+	s.send("PASS {}\r\n".format(config.PASS).encode("utf-8")) #Bot channel connect, replace with for loop with list
+	s.send("NICK {}\r\n".format(config.NICK).encode("utf-8")) #Bot host connect
 	main.CAP_REQ()
 	s.send("JOIN {}\r\n".format("#"+config.CHAN).encode("utf-8"))
 	s.send("JOIN {}\r\n".format("#"+config.NICK).encode("utf-8"))
@@ -29,9 +29,12 @@ def active_loop():
 			s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))	# Sending pong
 			print(config.NICK + ": Pong")	# Showing pong in console
 		elif "PRIVMSG" in response:
+			print(response)
 			reMessage = re.search(r"(PRIVMSG #(.*?) :(.*))", response)
-			username = reMessage.group(2)
+			reName = str(re.search(r"(display-name=(.*?;))", response).group(0))
+			username = reName[0:len(reName)-1]
 			message = reMessage.group(3)
+			channel = reMessage.group(2)
 			print(username+": "+message)
 			if re.match(config.CMDP, message[0]): # Checks for specified command character
 				main.valid_command(username, message,response)
--- main.py
+++ main.py
@@ -8,23 +8,25 @@ import urllib.request
 import urllib.error
 import json
 
+channel = config.CHAN
+
 def CAP_REQ():
 #	conn_handle.s.send(bytes('CAP REQ :twitch.tv/membership\n', 'UTF-8')) borks connection, will fix later
 	conn_handle.s.send(bytes('CAP REQ :twitch.tv/commands\n', 'UTF-8'))
 	conn_handle.s.send(bytes('CAP REQ :twitch.tv/tags\n', 'UTF-8'))
 
 def chat(msg):
-	conn_handle.s.send(bytes('PRIVMSG %s :%s\r\n' % ("#"+config.CHAN, msg), 'UTF-8'))
+	conn_handle.s.send(bytes('PRIVMSG %s :%s\r\n' % ("#"+channel, msg), 'UTF-8'))
 
-def ban(sock, user):
-	chat(".ban {}".format(user))
+def ban(user):
+	chat(".ban {}".format(user),channel)
 
-def timeout(sock, user, secs=60):
-	chat(".timeout {}".format(user, secs))
+def timeout(user, secs=60):
+	chat(".timeout {}".format(user, secs),channel)
 
 def valid_command(user, message, response):
 	command = re.search("(?<=\{0})\w+".format(config.CMDP), message)
 	if command:
 		command_list.perform_command(user, message, command, response)
 	else:
-		chat("Put in a command dummy KappaGee")
+		chat("Put in a command dummy KappaGee", channel)
